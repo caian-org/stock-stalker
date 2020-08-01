40a0de7c451be11b866398a0c721e163ebea8d0c
Caian R. Ertl <hi@caian.org>
2020-08-01 20:40:59 -0300

    feat: (WIP) main handler

---
+++ stalker/main.py
@@ -0,0 +1,23 @@
+from service.sheet import SheetWebApp
+from service.stock import StocksData
+from lib.env import environment as env
+
+
+def handler(event, context):
+    sheet = SheetWebApp(
+        env.get('SCRIPT_ID'),
+        env.get('SCRIPT_ACCESS_TOKEN'))
+
+    codes = []
+    transposed = {}
+
+    for ticker in sheet.get_tickers():
+        code = ticker['code']
+
+        codes.append(code)
+        transposed[code] = ticker
+
+    print(transposed)
+
+if __name__ == '__main__':
+    handler(None, None)
