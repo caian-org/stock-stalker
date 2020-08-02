1efd3291dbb721a45f23701441f506ad1b6dd59f
Caian R. Ertl <hi@caian.org>
2020-08-02 03:16:25 -0300

    feat: telegram notification main logic

--- stalker/__init__.py
+++ stalker/__init__.py
@@ -2,6 +2,7 @@ import json
 
 from stalker.service.sheet import SheetWebApp
 from stalker.service.stock import StocksData
+from stalker.service.telegram import TelegramBot
 from stalker.lib.env import environment as env
 
 
@@ -9,6 +10,10 @@ def handler(event, context):
     sheet = SheetWebApp(
         env.get('SCRIPT_ID'),
         env.get('SCRIPT_ACCESS_TOKEN'))
+
+    telegram = TelegramBot(
+        env.get('TELEGRAM_BOT_TOKEN'),
+        env.get('TELEGRAM_DEST_USER'))
 
     codes = []
     transposed = {}
@@ -20,7 +25,7 @@ def handler(event, context):
         transposed[code] = ticker
 
     tickers = []
-    to_notify = []
+    to_notify = {'buy': [], 'sell': []}
 
     for stocks in StocksData(codes).fetch():
         ticker = transposed.get(stocks.code)
@@ -28,11 +33,17 @@ def handler(event, context):
 
         if ticker['isBought']:
             if ticker['value'] >= ticker['expSell']:
-                to_notify.append(ticker)
+                to_notify['sell'].append(ticker)
         else:
             if ticker['value'] <= ticker['expBuy']:
-                to_notify.append(ticker)
+                to_notify['buy'].append(ticker)
 
         tickers.append(ticker)
 
     sheet.update(dict(tickers=tickers))
+
+    if to_notify['buy']:
+        telegram.notify_buy(to_notify['buy'])
+
+    if to_notify['sell']:
+        telegram.notify_sell(to_notify['sell'])
