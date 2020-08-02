9552790154d3f1ca1a3cbd055710498b3f82f6f2
Caian R. Ertl <hi@caian.org>
2020-08-02 01:57:05 -0300

    feat: method to update the sheet

--- stalker/service/sheet.py
+++ stalker/service/sheet.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 class SheetWebApp:
@@ -5,13 +6,24 @@ class SheetWebApp:
         self.base_url = f'https://script.google.com/macros/s/{sheet_id}/exec'
         self.access_token = access_token
 
-    def get_tickers(self):
-        res = requests.get(self.base_url, params={'accessToken': self.access_token})
+    def _auth(self):
+        return {'accessToken': self.access_token}
+
+    def _data(self, res):
         if res.status_code != 200:
             raise Exception('(SheetWebApp) Request failed. Got: {}'.format(res.content))
 
         res = res.json()
         if res['status'] != 200:
-            raise Exception('(SheetWebApp) Got status "{}"'.format(res['status']))
+            raise Exception('(SheetWebApp) Got status {}; Response is: {}'.format(
+                res['status'], json.dumps(res['data'])))
 
-        return res['data']
+        return res.get('data')
+
+    def get_tickers(self):
+        res = requests.get(self.base_url, params=self._auth())
+        return self._data(res)
+
+    def update(self, data):
+        res = requests.post(self.base_url, params=self._auth(), json=data)
+        return self._data(res)
