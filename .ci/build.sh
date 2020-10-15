881e0e013e9ac12fa9e5cb724aed5b8c026e4704
Caian R. Ertl <hi@caian.org>
2020-10-15 05:21:42 -0300

    chore: download the updated wheel file for pandas and numpy

--- .ci/build.sh
+++ .ci/build.sh
@@ -29,11 +29,11 @@ cp -r stalker $DIST
 cd $DIST
 rm -rf pandas numpy pip setuptools wheel
 curl \
-    "https://files.pythonhosted.org/packages/c2/76/73df80caf7affbe4b4f4a3b69a9a8f10b3b2acbb8179ad5bb578daaee56c/numpy-1.19.1-cp38-cp38-manylinux1_x86_64.whl" \
+    "https://files.pythonhosted.org/packages/41/6e/919522a6e1d067ddb5959c5716a659a05719e2f27487695d2a539b51d66e/numpy-1.19.2-cp38-cp38-manylinux1_x86_64.whl" \
     --output numpy.whl
 
 curl \
-    "https://files.pythonhosted.org/packages/5d/9f/f4c2a0f6f03d3ba95043c616e477926776ed3de7963a80edede7599630de/pandas-1.1.0-cp38-cp38-manylinux1_x86_64.whl" \
+    "https://files.pythonhosted.org/packages/64/0e/97fa348981b2ccebd39569200c91d587703329ea21508c30bb35110e404c/pandas-1.1.3-cp38-cp38-manylinux1_x86_64.whl" \
     --output pandas.whl
 
 unzip -o numpy.whl
