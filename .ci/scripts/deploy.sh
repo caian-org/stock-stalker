bef6c2a6e79b7a5e7e6f338f34daa173188e2944
Caian R. Ertl <hi@caian.org>
2020-10-17 00:23:52 -0300

    ci: use python 3.8

--- .ci/scripts/deploy.sh
+++ .ci/scripts/deploy.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-aws s3 cp "$ARTIFACT" "s3://${AWS_S3_BUCKET}/${ARTIFACT}"
+python3 -m aws s3 cp "$ARTIFACT" "s3://${AWS_S3_BUCKET}/${ARTIFACT}"
 
-aws lambda update-function-code \
+python3 -m aws lambda update-function-code \
    --function-name "$AWS_LAMBDA_NAME" \
    --s3-bucket "$AWS_S3_BUCKET" \
    --s3-key "$ARTIFACT"
